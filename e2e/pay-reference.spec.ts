e3c5343dec1a235f583e0604b04a7959f8e9fa19
Willian Monoga <bytewareteam@gmail.com>
2024-03-18 16:17:12 -0500

    setup validation test

--- e2e/pay-reference.spec.ts
+++ e2e/pay-reference.spec.ts
@@ -1,6 +1,18 @@
 import {expect, test} from "@playwright/test";
 
 test.describe('Referencias de pago', () => {
+
+    test('should validate reference code', async ({request}) => {
+        const REFERENCE_CODE = 'M';
+        const referenceResponse = await request.get(`api/TU/validarReferenciaPago?referenciaPago=${REFERENCE_CODE}`);
+        expect(referenceResponse.ok()).toBeFalsy();
+
+        const reference = await referenceResponse.json();
+        expect(reference).toEqual(expect.objectContaining({
+            Code: 'NOT OK'
+        }));
+    });
+
     test('Crear referencia de pago', async ({request}) => {
         const REFERENCE_CODE = '2934535052024031603';
         const referenceResponse = await request.get(`api/TU/getReferenciaPago?referenciaPago=${REFERENCE_CODE}`);

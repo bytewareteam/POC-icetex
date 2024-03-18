052be9de1a92a961855ee28ced9486205b74c409
Willian Monoga <bytewareteam@gmail.com>
2024-03-18 16:05:11 -0500

    setup reference pay test

--- e2e/pay-reference.spec.ts
+++ e2e/pay-reference.spec.ts
@@ -3,12 +3,14 @@ import {expect, test} from "@playwright/test";
 test.describe('Referencias de pago', () => {
     test('Crear referencia de pago', async ({request}) => {
         const REFERENCE_CODE = '1234';
-        const referenceResponse = await request.get(`api/TU/getReferenciaPago?refPago=${REFERENCE_CODE}`);
+        const referenceResponse = await request.get(`api/TU/getReferenciaPago?referenciaPago=${REFERENCE_CODE}`);
         expect(referenceResponse.ok()).toBeTruthy();
 
         const reference = await referenceResponse.json();
         expect(reference).toEqual(expect.objectContaining({
-            refPago: REFERENCE_CODE,
+            Data: expect.objectContaining({
+                ReferenciaPago: REFERENCE_CODE,
+            })
         }));
     });
 })
